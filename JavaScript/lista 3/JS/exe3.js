86e80d6fc9c56d8568d4f5b66b2342077cd4cd18
Felipe Campos <felipe.fcampos7@gmail.com>
2022-10-31 15:16:55 -0300

    v1.0.0.1

--- JavaScript/lista 3/JS/exe3.js
+++ JavaScript/lista 3/JS/exe3.js
@@ -3,7 +3,16 @@ function calcular(){
     const num1 = Number(document.querySelector('#numero1').value)
     const num2 = Number(document.querySelector('#numero2').value)
 
-    console.log(num1 + num2)
+    const res = document.querySelector('.resultado')
+
+    res.innerHTML= `
+        <p>
+        adição: ${adicao(num1, num2)} <br>
+        subtração: ${subtracao(num1, num2)} <br>
+        multiplicação: ${multiplicacao(num1, num2)} <br>
+        divisão: ${divisao(num1, num2)} <br>
+        </p>
+    `
 }
 
     function adicao(num1, num2) {
@@ -25,8 +34,3 @@ function calcular(){
         const div = num1 / num2
         return div   
     }
-
-    alert(`adição: ` + adicao(num1, num2)
-    `subtração: ` + subtracao(num1, num2)
-    `multiplicação: ` + multiplicacao(num1, num2)
-    `divisão: ` + divisao(num1, num2))
